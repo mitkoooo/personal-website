9fce2efccaf5fd3a6fedfd2fd141d49ef02c1499
Vadim Mitko <vmmmvmm@icloud.com>
2024-11-30 18:49:09 +0000

    Outlined implementation for dark mode determiner

---
+++ src/app/theme-determiner.ts
@@ -0,0 +1,11 @@
+// PRIORITY : LOCAL_STORAGE <-- SYSTEM <-- USER_TOOGLE
+
+// 1. Check whether there is preferred theme in the local storage
+
+// 2. Check whether the system is set to dark mode
+
+// 3. If not, set light mode on the document
+
+// 4. If yes, set dark mode on the document
+
+// 5. Save the theme setting to the local storage
