3e97cb9261411b21dc0df1d6ca2739aa63b1422e
Vadim Mitko <vmmmvmm@icloud.com>
2025-01-05 11:20:00 +0200

    Changed favicon and redesigned ol

--- src/app/(components)/Li.tsx
+++ src/app/(components)/Li.tsx
@@ -6,7 +6,7 @@ type LiProps = {
 }; /* use `interface` if exporting so that consumers can extend */
 
 const Li = ({ children }: LiProps): ReactElement => (
-  <li className="my-2 list-inside before:mr-2 before:content-['\2014'] dark:before:text-formal-grey">
+  <li className="relative my-2 list-inside pl-4 before:absolute before:-ml-4 before:mr-2 before:text-gray-400 before:content-['–'] dark:before:text-formal-grey">
     {children}
   </li>
 );
