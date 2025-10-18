0c5759fbb512ca2dec3f97ad4a6a3a4458d3c623
Vadim Mitko <vmmmvmm@icloud.com>
2025-10-18 19:43:25 +0100

    fix(ThemeToggle): Fix accessibility issues with ThemeToggle when using <Tab> to navigate the page

--- src/app/(components)/ThemeToggle.tsx
+++ src/app/(components)/ThemeToggle.tsx
@@ -19,19 +19,19 @@ const ThemeToggle = (): React.ReactNode => {
   } rounded-sm p-2.5 dark:p-[8px] cursor-pointer select-none`;
 
   return (
-    <>
+    <div className="flex flex-row items-center gap-7 text-center">
       <span className="hidden text-xs md:block md:text-sm">
         {isHovered && (isManuallyActivated ? theme : "system")}
       </span>
-      <span
+      <button
         className={`${ToggleCSS}`}
         onClick={handleClick}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         {theme === "dark" ? <MightySunIcon /> : <DarkMoonIcon />}
-      </span>
-    </>
+      </button>
+    </div>
   );
 };
 
